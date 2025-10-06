550690c3d89e53010b5e6f853c93b7edc780dcf1
paridhirustogi <68378029+paridhirustogi@users.noreply.github.com>
2025-10-06 14:09:30 +0000

    Deploying to gh-pages from @ paridhirustogi/paridhirustogi.github.io@56680590b52f53488b13b6fbd80f2e103d14b9aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -385,8 +385,8 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "news-more-bubbles-means-more-variation-in-ocean-carbon-storage",
-          title: 'More bubbles means more variation in ocean carbon storage',
+      },{id: "news-my-first-ph-d-paper-was-selected-as-a-research-spotlight-in-eos-more-bubbles-means-more-variation-in-ocean-carbon-storage",
+          title: 'My first Ph.D. paper was selected as a Research Spotlight in Eos: More...',
           description: "",
           section: "News",},{id: "news-attended-schmidt-futures-integration-across-models-and-observations-inmos-meeting-at-the-university-of-california-santa-barbara",
           title: 'Attended Schmidt Futures Integration Across Models and Observations (InMOS) meeting at the University...',
