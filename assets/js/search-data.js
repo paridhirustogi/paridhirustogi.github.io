a8e946c7e6a82c4a82093283d0da5b67202348b3
paridhirustogi <68378029+paridhirustogi@users.noreply.github.com>
2026-06-22 19:36:17 +0000

    Deploying to gh-pages from @ paridhirustogi/paridhirustogi.github.io@5e8d53253125ccefc282e5dc7dcc731408a21b91 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-bookshelf",
-          title: "Bookshelf",
+          title: "bookshelf",
           description: "",
           section: "Navigation",
           handler: () => {
