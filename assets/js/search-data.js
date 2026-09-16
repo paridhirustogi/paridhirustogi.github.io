28556b5ccb2373d765a6fe5474275cb3aaace192
paridhirustogi <68378029+paridhirustogi@users.noreply.github.com>
2026-09-16 22:01:57 +0000

    Deploying to gh-pages from @ paridhirustogi/paridhirustogi.github.io@5c1d9061f34546e2f9ff00506f634533f92a15db 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,7 +42,10 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_3/";
-            },},{id: "news-attended-schmidt-futures-integration-across-models-and-observations-inmos-meeting-at-the-university-of-california-santa-barbara",
+            },},{id: "news-i-was-invited-to-give-a-keynote-talk-about-the-inmos-project-at-the-schmidt-sciences-obvi-early-career-representative-conference-in-boston-on-september-8",
+          title: 'I was invited to give a keynote talk about the InMOS project at...',
+          description: "",
+          section: "News",},{id: "news-attended-schmidt-futures-integration-across-models-and-observations-inmos-meeting-at-the-university-of-california-santa-barbara",
           title: 'Attended Schmidt Futures Integration Across Models and Observations (InMOS) meeting at the University...',
           description: "",
           section: "News",},{id: "news-our-pnas-paper-on-bubble-driven-gas-exchange-featured-by-princeton-mechanical-aerospace-engineering",
